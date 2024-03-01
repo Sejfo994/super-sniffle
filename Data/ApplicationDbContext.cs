2fb0bb35250d419e360246565cfa90553ae4e842
Sejfo994 <159732465+Sejfo994@users.noreply.github.com>
2024-03-01 01:51:10 +0100

    Initialize revert-2-suyash

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using InventoryManagement0.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement0.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet <Inventory> InventoryTable { get; set; }
+
+        public DbSet <Login> LoginTable { get; set; }
+    }
+}
